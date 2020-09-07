3ad33335a163eade9220681512a0d07452796134
dgf-555 <844821247@qq.com>
2020-09-07 14:57:09 +0800

    src/exercise_02/characterCounter.js is ok

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,19 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
+  if(string ==''||string==null||string==undefined){
+    return 0;
+  }
+  else if(prediction==undefined){
+    return string.length;
+  }
+  else{
+    var answer = 0;
+    for(let i=0;i<string.length;i++){
+      if(prediction(string[i])==true)
+      answer++;
+    }
+    return answer;
+  }
 
-  throw new Error('Please delete this line and implement the function');
 }
