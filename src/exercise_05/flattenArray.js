c4fc33e4778860bbc428b65895386fcc84989688
dgf-555 <844821247@qq.com>
2020-09-07 16:25:33 +0800

    src/exercise_05/flattenArray.js is ok

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,18 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if(array==undefined||array==null)
+  throw new Error('Flatten undefined or null array');
+  var answer=[];
+  for(let i=0;i<array.length;i++){
+    if(typeof array[i] == "number"){
+      answer.push(array[i]);
+    }
+    else{
+      for(let j=0;j<array[i].length;j++){
+        answer.push(array[i][j]);
+      }
+    }
+  }
+  return answer;
 }
