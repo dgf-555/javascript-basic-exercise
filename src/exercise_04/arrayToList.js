14d82a8299571a5c6c6497fbaa7cdfb99dd80ba8
dgf-555 <844821247@qq.com>
2020-09-07 16:09:23 +0800

    src/exercise_04/arrayToList.js is ok

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,27 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if(array == undefined)
+  throw new Error('Creating list from undefined array');
+  if(array == null)
+  throw new Error('Creating list from undefined array');
+  if(array.length == 0)
+  throw new Error('Creating list from empty array');
+  var length = array.length;
+  var nodes =[];
+  for(let i=0;i<length;i++) {
+    var node = {};
+    node.value = array[i];
+    node.next = null;
+    nodes.push(node);
+  }
+  if(length>1){ 
+    for(let i=0;i<length-1;i++){
+    nodes[i].next=nodes[i+1];
+    }
+    return nodes[0];
+  }
+  else
+  return nodes[0];
 }
+
